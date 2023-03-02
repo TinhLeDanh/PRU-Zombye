0c62cf4d305f8f9a1bbc87dc2d1dc2bbda3eb7e3
Chu Van Trai (k15 HL) <traicvhe153014@fpt.edu.vn>
2023-03-02 21:35:44 +0700

    Update equipment interface and create site models

---
+++ Assets/Scripts/LogicWeapon/Model/Boots.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boots : MonoBehaviour
+{
+    public Sprite icon { get; set; }
+    public int damage { get; set; }
+    public float? cooldown { get; set; }
+    public int level { get; set; }
+}
---
+++ Assets/Scripts/LogicWeapon/Model/Shirt.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shirt
+{
+    public Sprite icon { get; set; }
+    public int damage { get; set; }
+    public float? cooldown { get; set; }
+    public int level { get; set; }
+}
