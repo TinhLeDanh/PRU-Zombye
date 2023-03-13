31efe6adecfec6f931b20a7a3cd8528f87ad2820
Chu Van Trai (k15 HL) <traicvhe153014@fpt.edu.vn>
2023-03-13 18:09:23 +0700

    load gamedata inventory

--- Assets/Scripts/GameData/Character/Character.cs
+++ Assets/Scripts/GameData/Character/Character.cs
@@ -7,5 +7,10 @@ public class Character : ScriptableObject
     public string Name;
     public float movementSpeed;
     public Weapon currentWeapon;
+    public Ring currentRing;
+    public Gloves currentGloves;
+    public Armor currentArmor;
+    public Pant currentPant;
+    public Shoes currentShoes;
     public Item[] items;
 }
--- Assets/Scripts/GameData/Item/Item.cs
+++ Assets/Scripts/GameData/Item/Item.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Item : ScriptableObject
 {
+    public Sprite icon;
     public int level;
     public string description;
 }
