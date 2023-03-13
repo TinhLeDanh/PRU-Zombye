444cde048734e902dd48f1acf26acf7cdc57c5a0
TinhLD <59137707+TinhLeDanh@users.noreply.github.com>
2023-03-13 12:17:59 +0700

    [movement] stopping distance

--- Assets/Scripts/Gameplay/EntityMovementSystems/RigidBodyEntityMovement.cs
+++ Assets/Scripts/Gameplay/EntityMovementSystems/RigidBodyEntityMovement.cs
@@ -87,7 +87,7 @@ public class RigidBodyEntityMovement : BaseGameEntityComponent<BaseGameEntity>, 
         if (movementMode == MovementMode.AutoMoveToTarget && Entity.target != null)
         {
             Vector3 targetPosition = Entity.target.transform.position;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * CurrentMoveSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition - Vector3.one, Time.deltaTime * CurrentMoveSpeed);
         }
     }
 
