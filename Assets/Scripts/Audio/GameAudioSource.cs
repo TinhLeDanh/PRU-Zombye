02026980c1a4c8b2b3c64c4f014c13ce56ee2764
thaidq <thaidqhe153058@fpt.edu.vn>
2023-03-21 21:42:35 +0700

    [audio] update audio manager

--- Assets/Scripts/Audio/GameAudioSource.cs
+++ Assets/Scripts/Audio/GameAudioSource.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameAudioSource : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Awake is called before Start
+    /// </summary>
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // make sure we only have one of this game object
+        // in the game
+        if (!AudioManager.Initialized)
+        {
+            // initialize audio manager and persist audio source across scenes
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            AudioManager.Initialize(audioSource);
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // duplicate game object, so destroy
+            Destroy(gameObject);
+        }
     }
 }
