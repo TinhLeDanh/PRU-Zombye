189efa1ba247646011adc481dca5919407c595e7
Le Danh Tinh <tinhtobe@gmail.com>
2023-03-24 02:50:38 +0700

    [gamedata] update game data

--- Assets/Scripts/Gameplay/UI/UIManager.cs
+++ Assets/Scripts/Gameplay/UI/UIManager.cs
@@ -38,6 +38,11 @@ public class UIManager : MonoBehaviour
         restartBtn.gameObject.SetActive(true);
     }
 
+    public void BackHomeScene()
+    {
+        SceneManager.LoadSceneAsync("GameplayScene");
+    }
+
     public void Restart()
     {
         SceneManager.LoadSceneAsync("GameplayScene");
